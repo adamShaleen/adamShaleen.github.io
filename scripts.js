104973202a954f12dfe04eb79c0f55226f6ed616
adamShaleen <adamshaleen@gmail.com>
2018-09-18 18:00:43 -0700

    blacking this out for now

--- scripts.js
+++ scripts.js
@@ -1,41 +1,47 @@
-$("#face").click(function () {
-    $(this).addClass('spin').one("animationend", function () {
-        $(this).removeClass('spin');
+const mainImage = "https://lh3.googleusercontent.com/fsPY29B3cFRUe1a5nFnAqYpSs9dsl-avpTw1-158oUbJYpM4oihxkzb1O4pMvFc_WtJDmzfVjLhpnl-cebr8s-Ybo_65kI12qC1HSltrmNgICba9D_xmYTcwJUmDDoIrfh8SJQBjExNjIs3q1tgfNh2yCHzVzLCaGXZd4pmmOYKrt-5nWkF37JFUBKmHtc4VUo7o03ql_pqDq8QFYpLA18wnOT7wZaSha3R4n46vk81cw7JmnDI0-w3lAySQwwtxOPEXcCZ23V-nMCozDKGjjN8VDWJR__BbNgTPhQzaWgYuVMgjJsx-fOlJgkp8S3lS1VUCtowuD4DeUZdvGVPuDwkswd1ZnnPhngUK-Wak81h_yGjBXYrLn_ooO3ek7KVZiZBMCoEjzVDeGI1KYhljuFeAKo0S251A5yYcgw0l3TCzv3ADQu3ExXBLArugxWDo2ptR3imIzv7pTTLVr-Nfl6NOyABlfotimaOTn1iunxBdXF3Ki5rAYo5tTmkQQEyObFrVD_HszL3JQ6O8Bp43dv5_Blmt0qRKfH9o53HVOsog6JmHmcd3xj7hsqkfYUL26F8BDRcnLdwJQFBsPDlE6Vx_dhGG0n64r99uyz9h0Ljn2VuuRcI6cp1JMdxil1ln=w576-h767-no";
+
+const aboutImage = "https://lh3.googleusercontent.com/f4yw4me35G40HvmN7RU28hUbDF8Jkn6sSqxTaep3uluXPRBHvqexUo3SeFAr79B5EHHi7Koy5QfGa_ubbd15DMgv7oYi1UBxJhgbSi2J0Nh2J3q0O8dgXqKyOjDenkL9WGgwCLyoryHm865jlpLIr6oOnCJkjYRjgT6n2M9mR4pfGRw85Or8oK3WCHsRe0Dsdt2oGXd7cxdqWCKUiWTf2cbiKao_3EErdtzCmp9lAEWX3_pfYmTsDr779-VmMj9bDrJlH8oaEyTcTiWoI-4XUFZAWNSBIcXQpgo99FDzK35Lyw15J4rhazs0B8tBS4wDJoo1XhJ6slRixkhawSPylh4Mnx5QJjI3DOu9EYBb7XBkjIHhj2Z-2O-Ltr97QXuA82yWjKXBsqbHWvQRiBv2iKwnHeKelIlQrhLqEDo5ugL8ZGQBEvR7SrWRE_7w1cW4d7GZYZnn8SMap1Ae5cFo85WIDs2IikfGjJtD3sakEgUntvzsTvwbzSefYYepXJjEjCQkGe6FZdmiBYX-6Vc8DGUTzKReNCQQniHWuKDuXB46O0lH_PP_UupMG-v998k0lD_c6t8qnn5aJfiIBrycz_z_pLK5LTdy5SvmHyS_3uvjLcLXFpjIaX23D38GAdu-=w1023-h767-no";
+
+const workImage = "https://lh3.googleusercontent.com/9QzLW03PI_abutJUu1Mw0JuUNm6fwUa15WTOTI1i6TR7XgjmWEngExEWrGIqXUYwO4zbTzm2VZHjkjM5ho5reXn4FdvitMGI3DuTZE0tAiON1gHb14RzrLJhBMB2U0TnuN6tyEdWzuvDOAV_-kQqWXNhx6E0-ZTXiJLoxx0Ek-IaMnuGz8UGqWOSzOLHl1FaeUokReQYB8Ps911-5u1znr4plfuK0tIN0xaEFNNTCGIqNgL49yZy4tlLjmQr_FyILSHDZwAnwvOepGfUKOCd1tCgYlbbUPCx0PlejmyjtXzJRZZLM7Mjnq8IMwvRLW8b0533D8vDOUFEQ-w1hkOHYgXQuWEtzQNPgKGxa4Ei5mkZj_2FvBV9CBkW0Bo4S07g_E4IZ9b0-HDheDlgCoHODvHppd88S4BoB-G0hip_GV4h6KY6OadaFjF5YsMgpm6f61OcGsppymhGXhWieTkSYFVm0Xn70qSdko-Ax5o0aLf9mpqUAdNttyOEHhB7Rx_5wgaEh3s9HGab0rBqo9D3NMMrbKZskX35TqsnvXNJ8akuHlq50ZC7Ve-SXyjlmvPvEPPTroxfPBdBv8iKJfDWkMu56OKIC5JAaodHNVN5itiIGc40FvGaiTlY5R27K6Et=w1023-h767-no";
+
+const funImage = "https://lh3.googleusercontent.com/VxQxcXqqSaUTR5PoZGQ1Q4QA9pNzPfBubuSe_PRBzFZ_fiaTfg3LFtZJu3w5XCMt7aThELBWnz6IPmrc_L2gyU6UVGHdCibeePzRynLMXAYFG_wrwq84gJZz_LYKat01sL_qc5wf3O7RJN8-aM_j1iYDvk9xHonZcCx6RlRqVFWQ1HyQth5KEgQspcvv2Se41ERE5ctkuB1FBDekZCSbeGfx5j5yTI4lzs99MHkrasiVOWxUKeuZvP8wZ-wmw-ccHmTovgy24NyOYAiqS4mbEZsdJ0tKTT6AchxJudUAPg98SjNsPGQIDRzemRoVRSG4nXJedz64ENeMlCdiPLhE-IZugiqX0gx8UHXNUytNgnV-JDzpVoyldFs2uEbiuSmp9JWrj30uYT4RxyTEwvUQDfeK76IFWIyJ-ffkj2LW0A14N3BsWa8B538wsZdpXGZkwx84VCPKLVt_ikOpMObCgVQ7tmUjJdspfM-gHUux3ouGAFiVW-SMQZnChmuAzg9hwFnC3-d-ZGTbCLt5iW9UmEKzI_LRuh-h2oscLnZaK-91fiHTJWHnxyc7poIib0XIdsK2eMIEGYiqY6PChlxEBiAxkPS9RCHtvy31MRFcYZMZS7SzFHB22-nZUdWuaUgN=w1023-h767-no";
+
+$(document).ready(function() {
+
+    $("#img").attr("src", mainImage);
+
+    $(".navTitles").click(function () {
+        $("#footer").fadeOut();
     });
-});
 
-$(".navTitles").click(function () {
-    $("#footer").fadeOut();
-});
+    $(".menuIcon").click(function () {
+        $("#footer").fadeIn();
+    });
 
-$(".menuIcon").click(function () {
-    $("#face").fadeIn();
-    $("#footer").fadeIn();
-});
+    $(".menuIcon").click(function () {
+        $(".menuIcon, .navTitles").toggleClass("openMenu");
+    });
 
-$(".menuIcon").click(function () {
-    $(".menuIcon, .navTitles").toggleClass("openMenu");
-});
+    $(".navTitles").click(function () {
+        $(".menuIcon, .navTitles").removeClass("openMenu");
+    });
 
-$(".navTitles").click(function () {
-    $(".menuIcon, .navTitles").removeClass("openMenu");
-});
+    $(".menuIcon").click(function () {
+        $("#aboutContent, #workContent, #funContent").fadeOut();
+    });
 
-$(".menuIcon").click(function () {
-    $("#aboutContent, #workContent, #funContent").fadeOut();
-});
+    $("#about").click(function () {
+        $("#img").attr("src", aboutImage);
+        $("#aboutContent").fadeIn();
+    });
 
-$("#about").click(function () {
-    $("#face").show();
-    $("#aboutContent").fadeIn();
-});
+    $("#work").click(function () {
+        $("#img").attr("src", workImage);
+        $("#workContent").fadeIn();
+    });
 
-$("#work").click(function () {
-    $("#face").show();
-    $("#workContent").fadeIn();
-});
-
-$("#fun").click(function () {
-    $("#face").hide();
-    $("#funContent").fadeIn();
-});
+    $("#fun").click(function () {
+        $("#img").attr("src", funImage);
+        $("#funContent").fadeIn();
+    });
+})
